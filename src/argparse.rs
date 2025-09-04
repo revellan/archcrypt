8caef709f4a95d15897d6a0d4295e713e6248d46
revellan <climb.simonschmidt@web.de>
2025-09-04 15:16:36 +0200

    Finish Parser

--- src/argparse.rs
+++ src/argparse.rs
@@ -1,52 +1,76 @@
+const ENCRYPT: &str = "--do-not-encrypt";
+const USERNAME: &str = "--username";
+const ROOT: &str = "--root-part-size";
+const HOME: &str = "--home-part-size";
+const SWAP: &str = "--swap-part-size";
+const HOME_SRC: &str = "--home-dir-source";
+const HOSTNAME: &str = "--hostname";
+const MOUNTPOINT: &str = "--mountpoint";
+const PARSING_ERROR: &str = "Parsing Error.";
+
 use super::Config;
+use core::panic;
 use revparse::{ArgState, Parser};
+fn parsenum<'a>(parser: &mut Parser<'a>, arg: &'a str) -> Option<i32> {
+    match parser.get(arg) {
+        ArgState::False => None,
+        ArgState::Value(val) => Some(Config::parse_disk_size(val)),
+        ArgState::True => panic!("{}", PARSING_ERROR),
+    }
+}
+fn parseopt<'a>(parser: &mut Parser<'a>, arg: &'a str) -> Option<String> {
+    match parser.get(arg) {
+        ArgState::False => None,
+        ArgState::Value(val) => Some(val),
+        ArgState::True => panic!("{}", PARSING_ERROR),
+    }
+}
 pub fn parse() -> Config {
     let mut parser = Parser::new("archcrypt");
     parser.add_argument(
-        "--home-dir-source",
-        Some("-s"),
-        "migrate home from DIRECTORY to new home",
-        Some("DIRECTORY"),
-    );
-    parser.add_argument(
-        "--do-not-encrypt",
+        ENCRYPT,
         Some("-d"),
         "do not encrypt the installation with luks",
         None,
     );
     parser.add_argument(
-        "--swap-part-size",
-        Some("-S"),
-        "size of the swap partition, sizes are explained in the documentation",
-        Some("SIZE"),
-    );
-    parser.add_argument(
-        "--home-part-size",
-        Some("-H"),
-        "size of the home partition, sizes are explained in the documentation",
-        Some("SIZE"),
-    );
-    parser.add_argument(
-        "--root-part-size",
-        Some("-H"),
-        "size of the root partition, sizes are explained in the documentation",
-        Some("SIZE"),
-    );
-    //parser.add_argument("--vg-name", Some("-v"), "name for lvm volume group", Some("VG_NAME"));
-    parser.add_argument(
-        "--hostname",
-        Some("-n"),
-        "hostname for installation",
-        Some("HOSTNAME"),
-    );
-    parser.add_argument(
-        "--username",
+        USERNAME,
         Some("-u"),
         "create user with username USERNAME and password 'password'",
         Some("USERNAME"),
     );
     parser.add_argument(
-        "--mountpoint",
+        ROOT,
+        Some("-H"),
+        "size of the root partition, sizes are explained in the documentation",
+        Some("SIZE"),
+    );
+    parser.add_argument(
+        HOME,
+        Some("-H"),
+        "size of the home partition, sizes are explained in the documentation",
+        Some("SIZE"),
+    );
+    parser.add_argument(
+        SWAP,
+        Some("-S"),
+        "size of the swap partition, sizes are explained in the documentation",
+        Some("SIZE"),
+    );
+    parser.add_argument(
+        HOME_SRC,
+        Some("-s"),
+        "migrate home from DIRECTORY to new home",
+        Some("DIRECTORY"),
+    );
+    parser.add_argument(
+        HOSTNAME,
+        Some("-n"),
+        "hostname for installation",
+        Some("HOSTNAME"),
+    );
+    parser.add_argument(
+        MOUNTPOINT,
         Some("-m"),
         "mountpoint to use during the installation process",
         Some("MOUNTPOINT"),
@@ -54,9 +78,19 @@ pub fn parse() -> Config {
     parser.add_pos_arg("DISK");
     parser.pos_arg_help("DISK to install the Operating System on.");
     parser.run();
-    match parser.get("--do-not-encrypt") {
-        ArgState::False => Config { encrypt: true },
-        ArgState::True => Config { encrypt: false },
-        ArgState::Value(_) => panic!("Parsing Error."),
-    }
+    let config: Config = Config {
+        encrypt: match parser.get(ENCRYPT) {
+            ArgState::False => true,
+            ArgState::True => false,
+            ArgState::Value(_) => panic!("{}", PARSING_ERROR),
+        },
+        home_src: parseopt(&mut parser, HOME_SRC),
+        root: parsenum(&mut parser, ROOT),
+        home: parsenum(&mut parser, HOME),
+        swap: parsenum(&mut parser, SWAP),
+        hostname: parseopt(&mut parser, HOSTNAME),
+        username: parseopt(&mut parser, USERNAME),
+        mountpoint: parseopt(&mut parser, MOUNTPOINT),
+    };
+    config
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,22 @@
+#[cfg(test)]
+mod tests;
 mod argparse;
 mod partitioner;
-struct Config {
+pub struct Config {
     encrypt: bool,
+    home_src: Option<String>,
+    root: Option<i32>,
+    swap: Option<i32>,
+    home: Option<i32>,
+    hostname: Option<String>,
+    username: Option<String>,
+    mountpoint: Option<String>,
+}
+impl Config {
+    fn parse_disk_size(size: String) -> i32 {
+        let _ = size;
+        4
+    }
 }
 pub fn install() {
     let conf: Config = argparse::parse();
